45e7260f18b4cad0d939f969fa97653321f577f3
Nikita <nikitaaa.dr@gmail.com>
2021-11-14 20:41:24 +0300

    lesson 22 is completed

--- scripts/script.js
+++ scripts/script.js
@@ -66,12 +66,13 @@ window.addEventListener('DOMContentLoaded', () => {
       target.closest('.menu, ul>li>a') && handleMenu();
 
       e.preventDefault();
-      menuItem.forEach((item, i) => {
-        if (item.childNodes[0] === target) {
-          handleMenu();
-          smoothScroll(target);
-        }
-      });
+
+      if (target.closest('a') && target.closest('menu')) {
+        handleMenu();
+        smoothScroll(target.closest('a'));
+      } else if (target.closest('a')) {
+        smoothScroll(target.closest('a'));
+      }
     });
   };
 
@@ -79,7 +80,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
   const toglePopup = () => {
     const btnPopup = document.querySelectorAll('.popup-btn'),
-      closePopup = document.querySelector('.popup-close'),
       popupModal = document.querySelector('.popup'),
       popupContent = document.querySelector('.popup-content');
 
@@ -260,4 +260,61 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   };
   calcValidate();
+
+  const calc = (price = 100) => {
+    const calcBlock = document.querySelector('.calc-block'),
+      calcType = document.querySelector('.calc-type'),
+      calcSquare = document.querySelector('.calc-square'),
+      calcDay = document.querySelector('.calc-day'),
+      calcCount = document.querySelector('.calc-count'),
+      totalValue = document.getElementById('total');
+
+    const animeValue = (total) => {
+      let n = 0;
+      const time = 20,
+        step = 10;
+
+      const t = Math.round(time / (total / step));
+
+      const interval = setInterval(() => {
+        n = n + step;
+        if (n == total) {
+          clearInterval(interval);
+        }
+        totalValue.textContent = n;
+      }, t);
+    };
+
+    const countSum = () => {
+      let total = 0,
+        countValue = 1,
+        dayValue = 1;
+
+      const typeValue = calcType.options[calcType.selectedIndex].value,
+        squareValue = +calcSquare.value;
+
+      if (calcCount.value > 1) {
+        countValue += (calcCount.value - 1) / 10;
+      }
+      if (calcDay.value && calcDay.value < 5) {
+        dayValue *= 2;
+      } else if (calcDay.value && calcDay.value < 10) {
+        dayValue *= 1.5;
+      }
+      if (typeValue && squareValue) {
+        total = price * typeValue * squareValue * countValue * dayValue;
+        animeValue(total);
+      }
+    };
+
+    calcBlock.addEventListener('change', (event) => {
+      const target = event.target;
+      
+      if (target.matches('select') || target.matches('input')) {
+        countSum();
+      }
+    });
+  };
+
+  calc(100);
 });
